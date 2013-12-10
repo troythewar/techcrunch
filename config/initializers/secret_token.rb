eb9dd00e0e8d7a8522d801eeb26335599347225c
troythewar <troythewar24@gmail.com>
2013-12-10 11:22:16 +0530

    Initial Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techcrunch::Application.config.secret_token = '352f6ea2ebf000ddba487cf8715f5f45d52c1d9d08dd20933273a38d29f2e7bf643b7121d7489d175092b3a23a7ab078e4b0330168299d3ce53b02be5860fc3f'
